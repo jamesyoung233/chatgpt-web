0c9c53a41a2f0d97e701a3f02e9dfe4f62a795b2
jamesyoung233 <630439118@qq.com>
2023-10-11 17:23:51 +0800

    Update helper.ts

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      avatar: 'https://raw.githubusercontent.com/jamesyoung233/jdscripts/blob/main/bingdundun.jpg',
+      name: 'jamesyoung233',
+      description: 'Star on <a href="https://github.com/jamesyoung233/chatgpt-web" class="text-blue-500" target="_blank" >GitHub</a>',
     },
   }
 }
